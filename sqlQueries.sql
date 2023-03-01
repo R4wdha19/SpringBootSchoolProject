ec630f89dd31ba002ce3459a0645d92a50acd6e2
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-03-01 18:39:00 +0400

    SQL

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -8,5 +8,12 @@
 		 select * from school
 		 select * from student
 	select c.* from course c where c.created_date >= '2022-02-25'
-	  Update school Set is_active = 0 where id = 1
+	  Update school Set is_active = 1 
+	  Update course Set is_active = 1 
+	  Update student Set is_active = 1 
+	  Update mark Set is_active = 1 
 	  Update school s Set s.isActive = 0 
+	   Update student Set updated_date = GETDATE() where updated_date is null;
+	   Update course Set updated_date = GETDATE() where updated_date is null;
+	   Update school Set updated_date = GETDATE() where updated_date is null;
+	   Update mark Set updated_date = GETDATE() where updated_date is null;
