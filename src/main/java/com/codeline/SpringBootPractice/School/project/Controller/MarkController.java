6b175abb8613ba35fd3ccae777e6eafa282f01c4
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-03-26 12:21:31 +0400

    Formating For Mark

--- src/main/java/com/codeline/SpringBootPractice/School/project/Controller/MarkController.java
+++ src/main/java/com/codeline/SpringBootPractice/School/project/Controller/MarkController.java
@@ -2,6 +2,7 @@ package com.codeline.SpringBootPractice.School.project.Controller;
 
 import com.codeline.SpringBootPractice.School.project.Model.Mark;
 import com.codeline.SpringBootPractice.School.project.Service.MarkService;
+import com.codeline.SpringBootPractice.School.project.Slack.SlackClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,62 +18,100 @@ import java.util.List;
 public class MarkController {
     @Autowired
     MarkService markService;
+    @Autowired
+    SlackClient slackClient;
+
 
     @RequestMapping(value = "getAll", method = RequestMethod.GET)
     public List<Mark> getAllMarks() {
         List<Mark> marks = new ArrayList<>();
         marks = markService.getAllMarks();
+        slackClient.sendMessage(markService.formatMarkListForSlack(marks).toString());
         return marks;
     }
 
     @RequestMapping(value = "getById", method = RequestMethod.GET)
     public Mark getMarkById(@RequestParam Integer markId) {
-        Mark mark = markService.getMarkById(markId);
-        return mark;
+        Mark markById = markService.getMarkById(markId);
+        slackClient.sendMessage(markService.formatMarkObjectForSlack(markById).toString());
+
+        return markById;
 
     }
 
     @RequestMapping(value = "getByGrade", method = RequestMethod.GET)
     public List<Mark> getMarkByGrade(@RequestParam String grade) {
-        List<Mark> mark = markService.getMarkByGrade(grade);
-        return mark;
+        List<Mark> markByGrade = markService.getMarkByGrade(grade);
+        slackClient.sendMessage(markService.formatMarkListForSlack(markByGrade).toString());
+        return markByGrade;
 
     }
 
     @RequestMapping(value = "getAllActiveMarks", method = RequestMethod.GET)
     public List<Mark> getAllActiveMarks() {
-        List<Mark> markList = new ArrayList<>();
-        markList = markService.getAllActiveMarks();
-        return markList;
+        List<Mark> allActiveMarkList = new ArrayList<>();
+        allActiveMarkList = markService.getAllActiveMarks();
+        slackClient.sendMessage(markService.formatMarkListForSlack(allActiveMarkList).toString());
+        return allActiveMarkList;
     }
 
 
     @RequestMapping(value = "getAllInActiveMarks", method = RequestMethod.GET)
     public List<Mark> getAllInActiveMarks() {
-        List<Mark> markList = new ArrayList<>();
-        markList = markService.getAllInActiveMarks();
-        return markList;
+        List<Mark> allInActiveMarkList = new ArrayList<>();
+        allInActiveMarkList = markService.getAllInActiveMarks();
+        slackClient.sendMessage(markService.formatMarkListForSlack(allInActiveMarkList).toString());
+        return allInActiveMarkList;
     }
 
     @RequestMapping(value = "getMarkCreatedAfterDate", method = RequestMethod.GET)
     public List<Mark> getMarkCreatedAfterDate(@RequestParam String createdDate) throws ParseException {
         List<Mark> markList = new ArrayList<>();
         markList = markService.getMarkCreatedAfterDate(createdDate);
+        slackClient.sendMessage(markService.formatMarkListForSlack(markList).toString());
         return markList;
     }
 
+
     @RequestMapping(value = "getLatestRow", method = RequestMethod.GET)
     public Mark getLatestRow() {
-        Mark mark = markService.getLatestRow();
-        return mark;
+        Mark markLatestRow = markService.getLatestRow();
+        slackClient.sendMessage(markService.formatMarkObjectForSlack(markLatestRow).toString());
+        return markLatestRow;
     }
 
     @RequestMapping(value = "getLatestUpdatedDate", method = RequestMethod.GET)
     public Mark getLatestUpdatedDate() {
-        Mark mark = markService.getLatestUpdatedDate();
-        return mark;
+        Mark markLatestUpdatedDate = markService.getLatestUpdatedDate();
+        slackClient.sendMessage(markService.formatMarkObjectForSlack(markLatestUpdatedDate).toString());
+        return markLatestUpdatedDate;
     }
 
+    @RequestMapping(value = "getByObtainedMarksMoreThan", method = RequestMethod.GET)
+    public List<Mark> getByObtainedMarksMoreThan(@RequestParam Integer obtainedMarks){
+        List<Mark> obtainedMarksMoreThan=markService.getByObtainedMarksMoreThan(obtainedMarks);
+        slackClient.sendMessage(markService.formatMarkListForSlack(obtainedMarksMoreThan).toString());
+        return obtainedMarksMoreThan;
+    }
+
+    @RequestMapping(value = "getByObtainedMarksLessThan", method = RequestMethod.GET)
+    public List<Mark> getByObtainedMarksLessThan(@RequestParam Integer obtainedMarks){
+        List<Mark> obtainedMarksLessThan=markService.getByObtainedMarksLessThan(obtainedMarks);
+        slackClient.sendMessage(markService.formatMarkListForSlack(obtainedMarksLessThan).toString());
+        return obtainedMarksLessThan;
+    }
+    @RequestMapping(value = "getMarksByUpdatedDate", method = RequestMethod.GET)
+    public List<Mark> getMarksByUpdatedDate(@RequestParam String updatedDate)  {
+        List<Mark> marksByUpdatedDate=markService.getMarksByUpdatedDate(updatedDate);
+        slackClient.sendMessage(markService.formatMarkListForSlack(marksByUpdatedDate).toString());
+        return marksByUpdatedDate;
+    }
+    @RequestMapping(value = "getMarksByCourseId", method = RequestMethod.GET)
+    public List<Mark> getMarksByCourseId(@RequestParam Integer courseId){
+        List<Mark> marksByCourseId=markService.getMarksByCourseId(courseId);
+        slackClient.sendMessage(markService.formatMarkListForSlack(marksByCourseId).toString());
+        return marksByCourseId;
+    }
     @RequestMapping(value = "deleteMarkById", method = RequestMethod.POST)
     public void deleteMarkById(@RequestParam Integer markId) {
         markService.deleteMarkById(markId);
@@ -89,28 +128,6 @@ public class MarkController {
         markService.deleteMarkByGrade(markGrade);
     }
 
-
-    @RequestMapping(value = "getByObtainedMarksMoreThan", method = RequestMethod.GET)
-    public List<Mark> getByObtainedMarksMoreThan(@RequestParam Integer obtainedMarks){
-        List<Mark> markList=markService.getByObtainedMarksMoreThan(obtainedMarks);
-        return markList;
-    }
-
-    @RequestMapping(value = "getByObtainedMarksLessThan", method = RequestMethod.GET)
-    public List<Mark> getByObtainedMarksLessThan(@RequestParam Integer obtainedMarks){
-        List<Mark> markList=markService.getByObtainedMarksLessThan(obtainedMarks);
-        return markList;
-    }
-    @RequestMapping(value = "getMarksByUpdatedDate", method = RequestMethod.GET)
-    public List<Mark> getMarksByUpdatedDate(@RequestParam String updatedDate)  {
-        List<Mark> markList=markService.getMarksByUpdatedDate(updatedDate);
-        return markList;
-    }
-    @RequestMapping(value = "getMarksByCourseId", method = RequestMethod.GET)
-    public List<Mark> getMarksByCourseId(@RequestParam Integer courseId){
-        List<Mark> markList=markService.getMarksByCourseId(courseId);
-        return markList;
-    }
     @RequestMapping(value = "deleteAllMarksCreatedAfterDate", method = RequestMethod.POST)
     public void deleteAllMarksCreatedAfterDate(@RequestParam String createdDate) throws ParseException {
         markService.deleteAllMarksCreatedAfterDate(createdDate);
