e8de8d98830c589fc4c2bda270280d1ab8b3382e
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-04-03 00:58:01 +0400

    Added More test cases for course

--- src/main/java/com/codeline/SpringBootPractice/School/project/Slack/SlackClient.java
+++ src/main/java/com/codeline/SpringBootPractice/School/project/Slack/SlackClient.java
@@ -7,12 +7,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class SlackClient {
 
     public String sendMessage(String text) {
-        return WebClient.create().post()
-                .uri("https://hooks.slack.com/services/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")
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(new SlackPayLoad(text))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        return "Done";
+//        return WebClient.create().post()
+//                .uri("https://hooks.slack.com/services/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")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .bodyValue(new SlackPayLoad(text))
+//                .retrieve()
+//                .bodyToMono(String.class)
+//                .block();
     }
 }
--- src/test/java/com/codeline/SpringBootPractice/School/project/Controller/CourseControllerTest.java
+++ src/test/java/com/codeline/SpringBootPractice/School/project/Controller/CourseControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
@@ -19,73 +20,97 @@ class CourseControllerTest {
     void testingTheCourseListSize() throws Exception {
         List<Course> listOfAllCoursesTest = courseController.getAllCourses();
         Integer listSize = listOfAllCoursesTest.size();
-        assertNotEquals(0,listSize);
+        assertNotEquals(0, listSize);
     }
 
     @Test
     void testingTheStudentIdFromGetCourseById() throws Exception {
-        Course courseById= courseController.getCourseById(1);
+        Course courseById = courseController.getCourseById(5);
         Integer studentId = courseById.getStudent().getId();
-        assertEquals(2,studentId);
+        assertEquals(3, studentId);
     }
+
     @Test
     void testingTheCourseNameFromGetCourseById() throws Exception {
-        Course courseById= courseController.getCourseById(1);
+        Course courseById = courseController.getCourseById(3);
         String courseName = courseById.getCourseName();
-        assertEquals("Kinder",courseName);
+        assertEquals("Math", courseName);
     }
+
     @Test
     void testingTheStatusFromGetCourseById() throws Exception {
-        Course courseById= courseController.getCourseById(1);
+        Course courseById = courseController.getCourseById(4);
         Boolean status = courseById.getIsActive();
         assertTrue(status);
     }
+
     @Test
     void testingTheUpdatedDateFromGetCourseById() throws Exception {
-        Course courseById= courseController.getCourseById(1);
+        Course courseById = courseController.getCourseById(9);
         Date updatedDate = courseById.getUpdatedDate();
-        assertEquals("2023-03-05 14:34:03.561",updatedDate.toString());
+        assertEquals("2023-03-02 12:33:31.04", updatedDate.toString());
     }
+
     @Test
     void testingSizeOfCoursesListFormGetCourseByName() {
-        List<Course> coursesByName = courseController .getCourseByName("English");
+        List<Course> coursesByName = courseController.getCourseByName("English");
         Integer listSize = coursesByName.size();
-        assertNotEquals(0,listSize);
+        assertNotEquals(0, listSize);
     }
+
     @Test
     void testingGettingTheCorrectNameFormGetCourseByName() {
-        List<Course> coursesByName = courseController .getCourseByName("English");
+        List<Course> coursesByName = courseController.getCourseByName("English");
         String courseName = coursesByName.get(0).getCourseName();
-     assertEquals("English",courseName);
+        assertEquals("English", courseName);
     }
+
     @Test
-    void testingCreatedDateIsNotNullFormGetCourseByName () {
-        List<Course> coursesByName = courseController .getCourseByName("English");
+    void testingCreatedDateIsNotNullFormGetCourseByName() {
+        List<Course> coursesByName = courseController.getCourseByName("English");
         Date createdDate = coursesByName.get(0).getCreatedDate();
         assertNotNull(createdDate);
     }
+
     @Test
     void testingGettingTheCorrectNumberOfCourseFormGetCourseByName() {
-        List<Course> coursesByName = courseController .getCourseByName("English");
+        List<Course> coursesByName = courseController.getCourseByName("English");
         Integer listSize = coursesByName.size();
-        assertEquals(3,listSize);
+        assertEquals(3, listSize);
     }
+
     @Test
-    void testingGettingAllActiveCourses() {
+    void testingTheListSizeAllActiveCourses() {
         List<Course> allActiveCourses = courseController.getAllActiveCourses();
         Integer sizeOfActiveCoursesList = allActiveCourses.size();
         assertNotNull(sizeOfActiveCoursesList);
     }
 
-
     @Test
-    void getAllInActiveCourses() {
-
+    void testingGettingAllActiveCourses() {
+        List<Course> allActiveCourses = courseController.getAllActiveCourses();
+        Boolean status = allActiveCourses.get(4).getIsActive();
+        assertTrue(status);
     }
 
     @Test
-    void getCourseCreatedAfterDate() {
+    void testingGettingAllInActiveCourses() {
+        List<Course> allInActiveCourses = courseController.getAllInActiveCourses();
+        Boolean status = allInActiveCourses.get(2).getIsActive();
+        assertFalse(status);
+    }
 
+    @Test
+    void testingTheListSizeAllInActiveCourses() {
+        List<Course> allInActiveCourses = courseController.getAllInActiveCourses();
+        Integer sizeOfInActiveCoursesList = allInActiveCourses.size();
+        assertNotNull(sizeOfInActiveCoursesList);
+    }
+
+    @Test
+    void getCourseCreatedAfterDate() throws ParseException {
+        List<Course> courseList = courseController.getCourseCreatedAfterDate("2023-03-05");
+        assertEquals(0,courseList.size());
     }
 
     @Test
